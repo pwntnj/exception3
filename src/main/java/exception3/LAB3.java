e159e1d802046d27d7689e32ba6013ea123b0215
Student <Student@5CD3019B19.control.ad.sit.kmutt.ac.th>
2023-03-28 14:54:00 +0700

    Exception part 1

---
+++ src/main/java/exception3/LAB3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception3;
+
+/**
+ *
+ * @author Student
+ */
+public class LAB3 {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Statement 1");
+            System.out.println("Statement 2");
+            System.out.println("Statement 3");
+        }
+        catch(ArithmeticException e){
+        }
+        finally {
+            System.out.println("Statement 4");
+        }
+        System.out.println("Statement 5");
+    }
+}
